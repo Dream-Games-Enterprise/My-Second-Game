a89030a54ad026a0c9e20995dacc86b00a118011
Dream Games Enterprise <dreamgames@dreamenterprise.info>
2024-11-03 19:55:19 +0000

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,389 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int maxHeight = 15;
+        public int maxWidth = 17;
+
+        public Color colour1;
+        public Color colour2;
+        public Color foodColour = Color.red;
+        public Color playerColour;
+
+        public Transform cameraHolder;
+
+        GameObject playerObject;
+        GameObject foodObject;
+        GameObject tailParent;
+        Node playerNode;
+        Node prevPlayerNode;
+        Node foodNode;
+        Sprite playerSprite;
+
+        GameObject mapObject;
+        SpriteRenderer mapRenderer;
+
+        Node[,] grid;
+        List<Node> availableNodes = new List<Node>();
+        List<SpecialNode> tail = new List<SpecialNode>();
+
+        bool up, left, right, down;
+
+        public float moveRate = 0.5f;
+        float timer;
+
+        Direction targetDirection;
+        Direction curDirection;
+
+        public enum Direction
+        {
+            up, left, right, down
+        }
+
+        #region Init
+
+        void Start()
+        {
+            CreateMap();
+            PlacePlayer();
+            PlaceCamera();
+            CreateFood();
+        }
+
+        void CreateMap()
+        {
+            mapObject = new GameObject("Map");
+            mapRenderer = mapObject.AddComponent<SpriteRenderer>();
+
+            grid = new Node[maxWidth, maxHeight];
+
+            Texture2D txt = new Texture2D(maxWidth, maxHeight);
+            for (int x = 0; x < maxWidth; x++)
+            {
+                for (int y = 0; y < maxHeight; y++)
+                {
+                    Vector3 tp = Vector3.zero;
+                    tp.x = x;
+                    tp.y = y;
+
+                    Node n = new Node()
+                    {
+                        x = x,
+                        y = y,
+                        worldPosition = tp
+                    };
+
+                    grid[x, y] = n;
+
+                    availableNodes.Add(n);
+
+                    #region Visual
+
+                    if (x % 2 != 0)
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, colour1);
+                        }
+                        else { txt.SetPixel(x, y, colour2); }
+                    }
+                    else 
+                    {
+                        if (y % 2 != 0)
+                        {
+                            txt.SetPixel(x, y, colour2);
+                        }
+                        else { txt.SetPixel(x, y, colour1); }
+                    }
+
+                    #endregion
+                }
+            }
+
+            txt.filterMode = FilterMode.Point;
+
+            txt.Apply();
+            Rect rect = new Rect(0, 0, maxWidth, maxHeight);
+            Sprite sprite = Sprite.Create(txt, rect, Vector2.zero, 1, 0, SpriteMeshType.FullRect);
+            mapRenderer.sprite = sprite;
+        }
+
+        void PlacePlayer()
+        {
+            playerObject = new GameObject("Player");
+            SpriteRenderer playerRenderer = playerObject.AddComponent<SpriteRenderer>();
+            playerSprite = CreateSprite(playerColour);
+            playerRenderer.sprite = playerSprite;
+            playerRenderer.sortingOrder = 1;
+            playerNode = GetNode(3, 3);
+            PlacePlayerObject(playerObject, playerNode.worldPosition);
+            playerObject.transform.localScale = Vector3.one * 1.2f;
+
+            tailParent = new GameObject("TailParent");
+        }
+
+        void PlaceCamera()
+        {
+            Node n = GetNode(maxWidth / 2, maxHeight / 2);
+            Vector3 p = n.worldPosition;
+            p += Vector3.one * 0.5f;
+            cameraHolder.position = p;
+        }
+
+        void CreateFood()
+        {
+            foodObject = new GameObject("Food");
+            SpriteRenderer foodRenderer = foodObject.AddComponent<SpriteRenderer>();
+            foodRenderer.sprite = CreateSprite(foodColour);
+            foodRenderer.sortingOrder = 1;
+            PlaceFood();
+        }
+
+        #endregion
+
+        void Update()
+        {
+            GetInput();
+            SetPlayerDirection();
+
+            timer += Time.deltaTime;
+            if (timer > moveRate)
+            {
+                timer = 0;
+                curDirection = targetDirection;
+                MovePlayer();
+            }
+        }
+        
+        void GetInput()
+        {
+            up = Input.GetButtonDown("Up");
+            down = Input.GetButtonDown("Down");
+            left = Input.GetButtonDown("Left");
+            right = Input.GetButtonDown("Right");
+        }
+
+        void SetPlayerDirection()
+        {
+            if (up)
+            {
+                SetDirection(Direction.up);
+            }
+            else if (down)
+            {
+                SetDirection(Direction.down);
+            }
+            else if (left)
+            {
+                SetDirection(Direction.left);
+            }
+            else if (right)
+            {
+                SetDirection(Direction.right);
+            }
+        }
+
+        void SetDirection(Direction d)
+        {
+            if (!isOppositeDir(d))
+            {
+                targetDirection = d;
+            }
+        }
+
+        void MovePlayer()
+        {
+            int x = 0;
+            int y = 0;
+
+            switch (curDirection)
+            {
+                case Direction.up:
+                    y = 1;
+                    break;
+                case Direction.down:
+                    y = -1;
+                    break;
+                case Direction.left:
+                    x = -1;
+                    break;
+                case Direction.right:
+                    x = 1;
+                    break;
+            }
+
+            Node targetNode = GetNode(playerNode.x + x, playerNode.y + y);
+            if (targetNode == null)
+            {
+                //game over
+            }
+            else
+            {
+                if (isTailNode(targetNode))
+                {
+                    //game over
+                }
+                else
+                {
+                    bool isScore = false;
+
+                    if (targetNode == foodNode)
+                    {
+                        isScore = true;
+                    }
+
+                    Node previousNode = playerNode;
+                    availableNodes.Add(previousNode);
+
+                    if (isScore)
+                    {
+                        tail.Add(CreateTailNode(previousNode.x, previousNode.y));
+                        availableNodes.Remove(previousNode);
+                    }
+
+                    MoveTail();
+
+                    PlacePlayerObject(playerObject, targetNode.worldPosition);
+                    playerNode = targetNode;
+                    availableNodes.Remove(playerNode);
+
+                    if (isScore)
+                    {
+                        if (availableNodes.Count > 0)
+                        {
+                            PlaceFood();
+                        }
+                        else { /*win */ }
+                    }
+                }
+            }
+        }
+
+        void MoveTail()
+        {
+            Node prevNode = null;
+
+            for (int i = 0; i < tail.Count; i++)
+            {
+                SpecialNode p = tail[i];
+                availableNodes.Add(p.node);
+
+                if (i == 0)
+                {
+                    prevNode = p.node;
+                    p.node = playerNode;
+                }
+                else
+                {
+                    Node prev = p.node;
+                    p.node = prevNode;
+                    prevNode = prev;
+                }
+
+                availableNodes.Remove(p.node);
+                PlacePlayerObject(p.obj, p.node.worldPosition);
+            }
+        }
+
+        #region Utilities
+
+        bool isOppositeDir(Direction d)
+        {
+            switch (d)
+            {
+                default:
+                case Direction.up:
+                    if (curDirection == Direction.down)
+                        return true;
+                    else
+                        return false;
+
+                case Direction.down:
+                    if (curDirection == Direction.up)
+                        return true;
+                    else
+                        return false;
+
+                case Direction.right:
+                    if (curDirection == Direction.left)
+                        return true;
+                    else
+                        return false;
+
+                case Direction.left:
+                    if (curDirection == Direction.right)
+                        return true;
+                    else
+                        return false;
+            }
+        }
+
+        bool isTailNode(Node n)
+        {
+            for (int i = 0; i < tail.Count; i++)
+            {
+                if (tail[i].node == n)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        void PlacePlayerObject(GameObject obj, Vector3 pos)
+        {
+            pos += Vector3.one * 0.5f;
+            obj.transform.position = pos;
+        }
+
+        void PlaceFood()
+        {
+            int ran = Random.Range(0, availableNodes.Count);
+            Node n = availableNodes[ran];
+            PlacePlayerObject(foodObject, n.worldPosition);
+            foodNode = n;
+        }
+
+        Node GetNode(int x, int y)
+        {
+            if (x < 0 || x > maxWidth - 1 || y < -0 || y > maxHeight - 1)
+            {
+                return null;
+            }
+
+            return grid[x, y];
+        }
+
+        SpecialNode CreateTailNode(int x, int y )
+        {
+            SpecialNode s = new SpecialNode();
+            s.node = GetNode(x, y);
+            s.obj = new GameObject();
+            s.obj.transform.parent = tailParent.transform;
+            s.obj.transform.position = s.node.worldPosition;
+            s.obj.transform.localScale = Vector3.one * 0.95f;
+            SpriteRenderer r = s.obj.AddComponent<SpriteRenderer>();
+            r.sprite = playerSprite;
+            r.sortingOrder = 1;
+
+            return s;
+        }
+
+        Sprite CreateSprite(Color targetColour)
+        {
+            Texture2D txt = new Texture2D(1, 1);
+            txt.SetPixel(0, 0, targetColour);
+            txt.Apply();
+            txt.filterMode = FilterMode.Point;
+            Rect rect = new Rect(0, 0, 1, 1);
+            return Sprite.Create(txt, rect, Vector2.one * 0.5f, 1, 0, SpriteMeshType.FullRect);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/SpecialNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SA
+{
+    public class SpecialNode
+    {
+        public Node node;
+        public GameObject obj;
+    }
+}
