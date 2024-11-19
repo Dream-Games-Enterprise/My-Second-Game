8f55d9318dc68b734884452360b8e9df9d9b106e
Dream Games Enterprise <dreamgames@dreamenterprise.info>
2024-11-19 14:36:59 +0000

    Added header toggle to sprite buying panels...

--- Assets/Scripts/SpriteManager.cs
+++ Assets/Scripts/SpriteManager.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SpriteManager : MonoBehaviour
 {
     [SerializeField] GameObject snakePanel;
     [SerializeField] GameObject tailPanel;
     [SerializeField] GameObject foodPanel;
+    [SerializeField] TMP_Text snakeHeader; //Should say "SNAKE HEAD"
+    [SerializeField] TMP_Text tailHeader; //Should say "SNAKE TAIL"
+    [SerializeField] TMP_Text foodHeader; //Should say "Food"
+
+    string pointDisplay;
+    Coroutine toggleCoroutine;
 
     void Start()
     {
-        DisablePanels();    
+        DisablePanels();
+        pointDisplay = "TOTAL POINTS " + PlayerPrefs.GetInt("currency").ToString();
     }
 
     public void DisablePanels()
@@ -19,10 +27,53 @@ public class SpriteManager : MonoBehaviour
         snakePanel.SetActive(false);
         tailPanel.SetActive(false);
         foodPanel.SetActive(false);
+
+        // Stop the toggle when all panels are closed
+        if (toggleCoroutine != null)
+        {
+            StopCoroutine(toggleCoroutine);
+            toggleCoroutine = null;
+        }
     }
 
     public void OpenPanel(GameObject panelToOpen)
     {
+        DisablePanels();
         panelToOpen.SetActive(true);
+
+        if (panelToOpen == snakePanel)
+        {
+            StartToggle(snakeHeader, "SNAKE HEAD");
+        }
+        else if (panelToOpen == tailPanel)
+        {
+            StartToggle(tailHeader, "SNAKE TAIL");
+        }
+        else if (panelToOpen == foodPanel)
+        {
+            StartToggle(foodHeader, "FOOD");
+        }
+    }
+
+    void StartToggle(TMP_Text header, string originalText)
+    {
+        // Stop any ongoing toggles
+        if (toggleCoroutine != null)
+        {
+            StopCoroutine(toggleCoroutine);
+        }
+        toggleCoroutine = StartCoroutine(ToggleHeaderText(header, originalText));
+    }
+
+    IEnumerator ToggleHeaderText(TMP_Text header, string originalText)
+    {
+        while (true)
+        {
+            header.text = originalText;
+            yield return new WaitForSeconds(1f);
+
+            header.text = pointDisplay;
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
